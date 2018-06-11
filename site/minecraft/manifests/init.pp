87ccb41df46ee317e48ac1280bb3b135615e77bc
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2018-06-12 00:49:23 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,8 @@ class minecraft {
    }
  file {'/opt/minecraft/minecraft_server.jar':
    ensure => file,
-   source => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar'
+   source => 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar',
+   before => Service['minecraft'],
    }
  package {'java':
    ensure => present,
@@ -20,6 +21,7 @@ class minecraft {
  service {'minecraft':
    ensure => running,
    enable => true,
+   require => [Package['java'],File['/opt/minecrft/eula.txt'],File['/etc/systemd/system/minecraft.service']],
    }
    
 }
